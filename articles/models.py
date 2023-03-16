0d507e860315247f71050a033d899387f2bf4843
sparsh2002 <ui20ec62@iiitsurat.ac.in>
2023-03-16 15:20:40 +0530

    models and ORM

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
+# run these commands as you update this file
+# python manage.py makemigrations
+# python manage.py migrate
+
 # Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField()
+    body = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+
+    # add in thumbnail later
+    # add in author late
+
+    def __str__(self):
+        return self.title
+
+
+
